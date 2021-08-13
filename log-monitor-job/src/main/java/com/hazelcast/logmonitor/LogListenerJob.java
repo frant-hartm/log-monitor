1a8472c175571f7c1abfe2bca1cd1cfe9e9b0b63
Frantisek Hartman <frant.hartm@gmail.com>
2021-08-13 12:44:50 +0200

    Create mapping if it doesn't exists

--- log-monitor-job/src/main/java/com/hazelcast/logmonitor/LogListenerJob.java
+++ log-monitor-job/src/main/java/com/hazelcast/logmonitor/LogListenerJob.java
@@ -8,6 +8,9 @@ import com.hazelcast.jet.Traversers;
 import com.hazelcast.jet.contrib.http.HttpListenerSources;
 import com.hazelcast.jet.pipeline.Pipeline;
 import com.hazelcast.jet.pipeline.Sinks;
+import com.hazelcast.sql.SqlResult;
+import com.hazelcast.sql.SqlRow;
+import com.hazelcast.sql.SqlService;
 
 import java.util.UUID;
 
@@ -21,6 +24,9 @@ public class LogListenerJob {
     public static void main(String[] args) {
         HazelcastInstance hz = Hazelcast.bootstrappedInstance();
         JetService jet = hz.getJet();
+        SqlService sql = hz.getSql();
+
+        createMappingIfNeeded(sql);
 
         Pipeline p = Pipeline.create();
 
@@ -39,5 +45,43 @@ public class LogListenerJob {
         items.writeTo(Sinks.map("logs", v -> UUID.randomUUID().toString(), v -> v));
 
         jet.newJob(p);
+    }
+
+    private static void createMappingIfNeeded(SqlService sql) {
+        boolean mappingExists = false;
+        try (SqlResult mappings = sql.execute("SHOW MAPPINGS")) {
+            for (SqlRow row : mappings) {
+                Object name = row.getObject("name");
+                if ("logs".equals(name)) {
+                    System.out.println("Mapping logs already exists.");
+                    mappingExists = true;
+                }
+            }
+        }
+
+        if (!mappingExists) {
+            SqlResult result = sql.execute("" +
+                    "CREATE MAPPING logs (\n" +
+                    "  __key VARCHAR,\n" +
+                    "  hostName VARCHAR,\n" +
+                    "  level VARCHAR,\n" +
+                    "  loggerClassName VARCHAR,\n" +
+                    "  loggerName VARCHAR,\n" +
+                    "  message VARCHAR,\n" +
+                    "  ndc VARCHAR,\n" +
+                    "  processId INT,\n" +
+                    "  processName VARCHAR,\n" +
+                    "  sequence INT,\n" +
+                    "  threadId INT,\n" +
+                    "  threadName VARCHAR,\n" +
+                    " \"timestamp\" VARCHAR\n" +
+                    ")\n" +
+                    "TYPE IMap\n" +
+                    "OPTIONS (\n" +
+                    "  'keyFormat' = 'varchar',\n" +
+                    "  'valueFormat' = 'json'\n" +
+                    ")");
+            System.out.println("Created mapping result: " + result.updateCount());
+        }
     }
 }
